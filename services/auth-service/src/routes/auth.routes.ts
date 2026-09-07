b6cb09f2e9bf3a704bc9d2eb3aa72f13cc715863
abdeali55 <abdealichallawala55@gmail.com>
2026-09-07 11:46:53 +0530

    feat: scaffold gateway service and implement auth service registration schema and routes

--- services/auth-service/src/routes/auth.routes.ts
+++ services/auth-service/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { registerHandler } from "@/controllers/auth.controller";
 import { validateRequest } from "@chatapp/common";
 import { Router } from "express";
+import { registerSchema } from "@/routes/auth.schema";
 
 
 
 export const authRouter: Router = Router();
 
-authRouter.post("/register", validateRequest({}), registerHandler);
+authRouter.post("/register", validateRequest({body: registerSchema.shape.body}), registerHandler);
 
--- services/gateway-service/src/app.ts
+++ services/gateway-service/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Application } from "express";
 import cors from "cors"
 import helmet from "helmet"
 import { errorHandler } from "@/middleware/error-handler";
-import { registerRoutes } from "@/routes";
+
 
 export const createApp = (): Application => {
     const app = express();
@@ -11,8 +11,6 @@ export const createApp = (): Application => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
-    registerRoutes(app);
-    
 
     app.use((_req, res) => {
         res.status(404).json({message: "Not Found"});
